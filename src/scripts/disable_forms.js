46b3cfa5e0f6e217905d972b916efade92558b9b
Nathan Fisher <nfisher.sr@gmail.com>
2021-01-14 23:12:21 -0500

    Revised method for blocking keypresses in Insert mode

--- src/scripts/disable_forms.js
+++ src/scripts/disable_forms.js
@@ -1,8 +1,3 @@
-inputs = document.getElementsByTagName('input');
-for (i = 0; i < inputs.length; i++) {
-    inputs[i].disabled = true;
-}
-edits = document.getElementsByTagName('textarea');
-for (i = 0; i < edits.length; i++) {
-    edits[i].disabled = true;
+document.onkeydown = function (e) {
+  return false;
 }
